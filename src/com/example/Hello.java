7699b9849049f0ec2693c137140843e0b704fdf4
Mathankumar P <Mathankumar1127@gmail.com>
2023-09-30 10:28:35 +0530

    Update Hello.java

--- src/com/example/Hello.java
+++ src/com/example/Hello.java
@@ -1,9 +1,19 @@
-/**
- * This is a Hello world program.
- */
 package com.example;
 
-public class Hello {
+/**
+ * Utility class for greeting messages.
+ */
+public final class Hello {
+
+    private Hello() {
+        // private constructor to prevent instantiation
+    }
+
+    /**
+     * Prints "Hello, World!" to the console.
+     * 
+     * @param args Command line arguments (not used in this method).
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
